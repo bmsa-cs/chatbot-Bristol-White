e76c06c12fa1d8dc9e262bc3b745046237983be6
Bristol White <bwhite24@biomedscienceacademy.org>
2021-01-20 16:06:33 +0000

    Added DOB prediction.

--- main.py
+++ main.py
@@ -32,9 +32,47 @@ def main():
     print("Ah,",str(age),"is a good age! Old enough to drive, nice!")
   elif age == 16:
     print("Ah,",str(age),"is a good age! Old enough to get your drivers permit, nice!")
+  elif age >= 21:
+    print("Ah,",str(age),"is a good age! Old enough to drink and drive! (But don't combine those two things please!)")
   else:
     print("Ah,",str(age),"is a good age!")
   time.sleep(0.5)
+  print("Bet I can guess what year you were born.")
+  time.sleep(0.3)
+  year = 2021 - age
+  if year <= 0:
+    print("Hey, wait a minute, you shouldn't be alive!")
+  else:
+    print("Let's see here... according to my calculations you were born in", year,)
+  answer = input("Was I correct? (y/n)")
+  if answer == "y":
+    print("Easy, a robot never fails!")
+  if answer == "n":
+    answer_two = input("Darn, was I close at least? (y/n)")
+    if answer_two == "y":
+      print("Well that's good! Robots aren't perfect afterall!")
+    if answer_two == 'n':
+      print("Ah, that's unlucky. Robots aren't perfect.")
+    else:
+      print("Sorry, I don't quite understand what you said. Try again pelase!")
+      answer_two = input("Darn, was I close at least? (y/n)")
+      if answer_two == "y":
+        print("Well that's good! Robots aren't perfect afterall!")
+      if answer_two == 'n':
+        print("Ah, that's unlucky. Robots aren't perfect.")
+  else:
+    print("Sorry, I don't quite understand what you said. Try again please!")
+    answer = input("Was I correct? (y/n)")
+    if answer == "y":
+      print("Easy, a robot never fails!")
+    if answer == "n":
+      answer_two = input("Darn, was I close at least? (y/n)")
+      if answer_two == "y":
+        print("Well that's good! Robots aren't perfect afterall!")
+      if answer_two == 'n':
+        print("Ah, that's unlucky. Robots aren't perfect.")
+      else:
+        print("Sorry, I don't quite understand what you said. Try again pelase!")
 
 
 if __name__ == "__main__":
